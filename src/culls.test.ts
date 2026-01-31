e1df5494f75b127e13bdd1c06ea5b1b79c3bf1e7
Eric Liu <ericyl.us@gmail.com>
2026-01-30 23:33:19 -0800

    chore: use bun to build

--- src/culls.test.ts
+++ src/culls.test.ts
@@ -1,7 +1,7 @@
 import  { describe, it, expect, beforeEach, afterEach, spyOn, mock } from "bun:test";
 import fs from "node:fs";
 import path from "node:path";
-import { culls } from "../src/culls";
+import { culls } from "./culls";
 
 describe("package.json cleaner", () => {
   const originalCwd = process.cwd;
