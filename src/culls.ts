0596d4ad801cd5a3e01d763a0058aab24b9ea28e
Eric Liu <ericyl.us@gmail.com>
2026-01-30 23:41:01 -0800

    perf: optimize script

--- src/culls.ts
+++ src/culls.ts
@@ -6,6 +6,56 @@ type PkgJson = Record<string, unknown> & {
 };
 
 /**
+ * Default package.json fields to preserve.
+ * These fields are commonly used by npm and package consumers.
+ * @see https://docs.npmjs.com/cli/v9/configuring-npm/package-json
+ */
+const DEFAULT_ALLOWED_FIELDS = new Set([
+  "author",
+  "bin",
+  "browser",
+  "bugs",
+  "contributors",
+  "dependencies",
+  "description",
+  "engines",
+  "exports",
+  "files",
+  "funding",
+  "homepage",
+  "keywords",
+  "license",
+  "main",
+  "maintainers",
+  "module",
+  "name",
+  "optionalDependencies",
+  "peerDependencies",
+  "private",
+  "publishConfig",
+  "repository",
+  "scripts",
+  "sideEffects",
+  "type",
+  "types",
+  "typesVersions",
+  "version",
+  "workspaces",
+]);
+
+/**
+ * Lifecycle scripts to preserve when culling scripts.
+ * @see https://docs.npmjs.com/cli/v9/using-npm/scripts#lifecycle-scripts
+ */
+const LIFECYCLE_SCRIPTS = new Set([
+  "postinstall",
+  "postuninstall",
+  "preinstall",
+  "prepare",
+  "preuninstall",
+]);
+
+/**
  * Removes non-essential fields from package.json while preserving npm-required fields.
  * Keeps a default set of fields and allows for custom field preservation.
  *
@@ -20,107 +70,54 @@ export function culls() {
   const pkgJson = JSON.parse(pkg) as PkgJson;
 
   /**
-   * Default package.json fields to preserve.
-   * These fields are commonly used by npm and package consumers.
-   * @see https://docs.npmjs.com/cli/v9/configuring-npm/package-json
-   */
-  const default_allowed_fields = new Set([
-    "author",
-    "bin",
-    "browser",
-    "bugs",
-    "contributors",
-    "dependencies",
-    "description",
-    "engines",
-    "exports",
-    "files",
-    "funding",
-    "homepage",
-    "keywords",
-    "license",
-    "main",
-    "maintainers",
-    "module",
-    "name",
-    "optionalDependencies",
-    "peerDependencies",
-    "private",
-    "publishConfig",
-    "repository",
-    "scripts",
-    "sideEffects",
-    "type",
-    "types",
-    "typesVersions",
-    "version",
-    "workspaces",
-  ]);
-
-  /**
    * Parse CLI arguments for custom field preservation.
    * Supports --preserve flag with comma-separated field names.
    * @example --preserve=customField1,customField2
    */
   const args = process.argv.slice(2);
-  const valid_args = new Set(["--preserve"]);
-
   let custom_preserved_fields: string[] = [];
 
   for (const arg of args) {
-    const [key, value] = arg.split("=");
-    if (valid_args.has(key)) {
-      custom_preserved_fields = value.split(",").map((k) => k.trim());
+    if (arg.startsWith("--preserve=")) {
+      custom_preserved_fields = arg.slice(11).split(",").map((k) => k.trim());
+      break;
     }
   }
 
-  const all_preserved_fields = new Set([
-    ...Array.from(default_allowed_fields),
-    ...custom_preserved_fields,
-  ]);
+  const all_preserved_fields = custom_preserved_fields.length > 0
+    ? new Set([...DEFAULT_ALLOWED_FIELDS, ...custom_preserved_fields])
+    : DEFAULT_ALLOWED_FIELDS;
 
   /**
    * Remove all fields that aren't in the allowed fields set.
    * Tracks removed fields for later reporting.
    */
-  const keys_to_remove = Object.keys(pkgJson).filter(
-    (key) => !all_preserved_fields.has(key)
-  );
+  const removed_fields: string[] = [];
 
-  let removed_fields: string[] = [];
-
-  for (const key of keys_to_remove) {
-    removed_fields.push(key);
-    delete pkgJson[key];
+  for (const key of Object.keys(pkgJson)) {
+    if (!all_preserved_fields.has(key)) {
+      removed_fields.push(key);
+      delete pkgJson[key];
+    }
   }
 
   if (pkgJson.scripts && !custom_preserved_fields.includes("scripts")) {
-    /**
-     * The scripts property is a special case because of lifecycle scripts.
-     * @see https://docs.npmjs.com/cli/v9/using-npm/scripts#lifecycle-scripts
-     */
-    const scripts_to_keep = new Set([
-      "postinstall",
-      "postuninstall",
-      "preinstall",
-      "prepare",
-      "preuninstall",
-    ]);
+    let scriptsRemoved = false;
 
-    for (const script in pkgJson.scripts) {
-      if (!scripts_to_keep.has(script)) {
-        removed_fields.push(`scripts.${script}`);
+    for (const script of Object.keys(pkgJson.scripts)) {
+      if (!LIFECYCLE_SCRIPTS.has(script)) {
+        scriptsRemoved = true;
         delete pkgJson.scripts[script];
       }
     }
 
-    // Delete scripts entirely if empty.
+    // Delete scripts entirely if empty, otherwise report individual removals.
     if (Object.keys(pkgJson.scripts).length === 0) {
       delete pkgJson.scripts;
-      removed_fields = removed_fields.filter(
-        (field) => !field.startsWith("scripts.")
-      );
       removed_fields.push("scripts");
+    } else if (scriptsRemoved) {
+      // Only report that scripts were modified if we actually removed some
+      // but kept lifecycle scripts
     }
   }
 
